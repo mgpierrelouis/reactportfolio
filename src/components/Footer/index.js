0218b6db6bf2e85acdd66068730765da1e0b358c
Michael Pierre-Louis <82857756+mgpierrelouis@users.noreply.github.com>
2021-09-27 05:05:18 -0400

    updated color of text and buttons on page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,23 +7,23 @@ function Footer() {
             <div className= "container" style={{display: 'flex', justifyContent: 'center'}}>
                 <div className= "row col-lg-12" style={{display: 'flex', justifyContent: 'center'}}>
                     <div className= "col-lg-4 col-md-4 col-sm-4">
-                        <h3 className="text-center">Contact</h3>
+                        <h3 className="text-center" style= {{color:'white'}}>Contact</h3>
                         <ul className= "list-unstyled" style={{justifyContent: 'center'}}>
-                            <li className="text-center">mgpierrelouis@gmail.com</li>
-                            <li className="text-center">980.267.2600</li>
+                            <li className="text-center" style= {{color: 'white'}}>mgpierrelouis@gmail.com</li>
+                            <li className="text-center"style= {{color: 'white'}}>980.267.2600</li>
                         </ul>                        
                     </div>
                     <div className= "col-lg-4 col-md-4 col-sm-4" style={{justifyContent: 'center'}}>
-                        <h2 className="text-center">Michael Pierre-Louis</h2>
-                        <p className= "text-center">© Copyright 2021 - Michael Pierre-Louis. All Rights Reserved</p>                        
+                        <h2 className="text-center" style={{color: 'white'}}>Michael Pierre-Louis</h2>
+                        <p className= "text-center" style={{color: 'white'}}>© Copyright 2021 - Michael Pierre-Louis. All Rights Reserved</p>                        
                     </div>
                     <div className= "col-lg-4 col-md-4 col-sm-4">
-                        <h3 className="text-center">Websites</h3>
+                        <h3 className="text-center" style={{color: 'white'}}>Websites</h3>
                         <ul className= "list-unstyled" style={{justifyContent: 'center'}}>
-                            <li className="text-center">
+                            <li className="text-center" style= {{color: 'white'}}>
                                 <a href = "https://github.com/mgpierrelouis/">Github</a>
                             </li>
-                            <li className = "text-center">
+                            <li className = "text-center" style= {{color: 'white'}}>
                                 <a href = "https://www.linkedin.com/in/michael-pierre-louis-7163b2bb/">Linkedin</a>
                             </li>
                         </ul>                        
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import ProjectInfo from './projectsinfo.json'
 import 'bootstrap/dist/css/bootstrap.css'
+import './index.css'
 
 
 
@@ -11,15 +12,15 @@ function Project() {
         <div className= 'container'>
             {data.map((d)=> (
                     <div className="jumbotron text-white jumbotron-image shadow" style= {{backgroundImage: `url("https://images.unsplash.com/photo-1552152974-19b9caf99137?fit=crop&w=1350&q=80")`}} >
-                    <h2 className="mb-4 text-center" style= {{color: "orange"}}>
+                    <h2 className="mb-4 text-center" style= {{color: "green"}}>
                       {d.title}
                     </h2>
-                    <p className="mb-4 text-center" style= {{color: "magenta"}}>
+                    <p className="mb-4 text-center" style= {{color: "green"}}>
                       {d.description}
                     </p>
                     <div style = {{display: "flex", justifyContent: "center"}}>
-                    <a href={d.app} className="btn btn-primary">Deployed Application</a>
-                    <a href={d.github} className="btn btn-primary">Github Repository</a>
+                    <a href={d.app} className="custom-btn btn btn-primary">Deployed Application</a>
+                    <a href={d.github} className="custom-btn btn btn-primary">Github Repository</a>
                     </div>
                   </div>
                     ))}
